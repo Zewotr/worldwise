900b0c87f59f225c28b29c6c44cd750f2e28b3f2
Zewotr <zewotrl@gmail.com>
2026-02-07 17:12:13 +0300

    update Map file

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,15 @@
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
 export default function Map() {
+    const [searchParams, setSearchParams] =useSearchParams();
+    const lat = searchParams.get("lat");
+    const lng = searchParams.get("lng");
+    const navigate = useNavigate();
   return (
-    <div className={styles.mapContainer}>
-
+    <div className={styles.mapContainer} onClick={()=>navigate("form")}>
+        <h2>Map</h2>
+        <p>Latitude: {lat}</p>
+        <p>Longitude: {lng}</p>
     </div>
   )
 }
