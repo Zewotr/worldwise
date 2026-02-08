b4f51f34f1f52e587151020334f9f5c1ba99502b
Zewotr <zewotrl@gmail.com>
2026-02-09 02:27:01 +0300

    create new custom hook

---
+++ src/hooks/useGeolocation.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export function useGeolocation(defaultPosition= null) {
+    const [isLoading, setIsLoading] = useState(false);
+    const [position, setPosition] = useState(defaultPosition);
+    const [error, setError] = useState(null);
+
+    function getPosition(){
+        if (!navigator.geolocation) {
+            return setError("Geolocation is not supported by your browser");
+        }
+        setIsLoading(true);
+        navigator.geolocation.getCurrentPosition(
+            (position)=>{
+                setPosition({
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                });
+                setError("");
+                setIsLoading(false);
+            },
+            (error)=>{
+                setError(error.message);
+                setIsLoading(false);
+            }
+        );
+    }
+
+  return {isLoading, position, error, getPosition};
+}
