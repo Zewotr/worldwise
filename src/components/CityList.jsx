051e87e4179f100c95c8398727d57b9e5384616a
Zewotr <zewotrl@gmail.com>
2026-02-09 02:21:40 +0300

    update file

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,7 +2,9 @@ import Spinner from "./Spinner";
 import CityItem from "./CityItem";
 import Message from "./Message";
 import styles from './CityList.module.css'
-export default function CityList({cities, isLoading}) {
+import { useCities } from "../contexts/CityContext";
+export default function CityList() {
+    const {cities, isLoading} = useCities();
 
     if (isLoading) return <Spinner />;
     if (!cities.length) return <Message message="Add your first city by clicking on the map!" />;
