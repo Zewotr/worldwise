64bfd2acdc509ca9392c1f4d6acc1b5304488f1c
Zewotr <zewotrl@gmail.com>
2026-02-09 02:22:38 +0300

    update file

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,7 +1,9 @@
 import Spinner from "./Spinner";
 import CountryItem from "./CountryItem";
 import styles from './CountryList.module.css'
-export default function CountryList({cities, isLoading}) {
+import { useCities } from "../contexts/CityContext";
+export default function CountryList() {
+    const {cities, isLoading} = useCities();
     if (isLoading) return <Spinner />;
     
     const countries = cities.reduce((arr, city)=>{if(!arr.map(el=>el.country).includes(city.country)) return [...arr, {country: city.country, emoji: city.emoji}]; else return arr},[]);
