681b16212311b3e7dffa8aa0d284d403f9180813
Zewotr <zewotrl@gmail.com>
2026-02-10 02:46:28 +0300

    update file

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,14 +1,14 @@
-import AppNav from "../components/AppNav.jsx"
 import Sidebar from "../components/Sidebar.jsx"
 import Map from "../components/Map.jsx"
 
 import styles from './AppLayout.module.css'
+import User from "../components/User.jsx"
 export default function AppLayout() {
   return (
     <div className={styles.app}>
-    {/* <AppNav /> */}
     <Sidebar />
     <Map />
+    <User />
     </div>
   )
 }
