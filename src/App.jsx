0877d1b8610184feab7384d4150db5ca11f3ac54
Zewotr <zewotrl@gmail.com>
2026-02-10 02:47:06 +0300

    update file

--- src/App.jsx
+++ src/App.jsx
@@ -9,28 +9,39 @@ import City from "./components/City.jsx"
 import CityList from "./components/CityList.jsx"
 import CountryList from "./components/CountryList.jsx";
 import Form from "./components/Form.jsx"
+import { AuthProvider } from "./contexts/FakeAuthContext.jsx";
+import ProtectedRoute from "./pages/ProtectedRoute.jsx";
 
 function App() {
    
 
   return (
     <CitiesProvider>
+        <AuthProvider>
         <BrowserRouter> 
             <Routes>
                 <Route path="/" element={<Homepage />} />
                 <Route path="/product" element={<Product />} />
                 <Route path="/pricing" element={<Pricing />} />
-                <Route path="/app" element={<AppLayout />}>
+                <Route path="/app" 
+                        element={ 
+                        <ProtectedRoute > 
+                            <AppLayout /> 
+                        </ProtectedRoute>}
+                        >
                     <Route index element={<Navigate replace to="cities" /> } />
                     <Route path="cities" element={<CityList />} />
                     <Route path="countries" element={<CountryList /> } />
                     <Route path="cities/:id" element={<City />} />
                     <Route path="form" element={<Form /> } />
                 </Route>
+                
                 <Route path="/login" element={<Login />} />
+                
                 <Route path="*" element={<h2>Page Not Found</h2>} />
             </Routes>   
         </BrowserRouter>
+        </AuthProvider>
     </CitiesProvider>
    );
 }
