a38fd4d951c3810090c3ac2b4e97b208ebd8d032
Zewotr <zewotrl@gmail.com>
2026-02-10 02:43:15 +0300

    update file

---
+++ src/contexts/FormContext.jsx
@@ -0,0 +1,16 @@
+// import { latLng } from "leaflet";
+// import { createContext, useContext } from "react"
+
+// const FormContext = createContext({lat: 40, lng: 0});
+// function FormProvider() {
+//     const context = useContext(FormContext);
+//     if (!context) throw new Error("FormContext was used outside of FormContextProvider");
+
+
+//   return 
+//     <FormContext.Provider value={latLng} >
+//         {children}
+//     </FormContext.Provider>
+// }
+
+// export {FormProvider, FormContext};
