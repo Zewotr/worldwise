869e316f241dd459630a9f67d7c63fe6f818573c
Zewotr <zewotrl@gmail.com>
2026-02-10 02:47:50 +0300

    update file

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -10,8 +10,13 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 export default function CityItem({city}) {
-    const { curentCity } = useCities();
+    const { curentCity, deleteCity } = useCities();
     const { cityName, emoji, date,id, position } = city;
+
+    function handleDelete(e) {
+        e.preventDefault();
+        deleteCity(id);
+    }
 
   return (
     <li>
@@ -19,7 +24,7 @@ export default function CityItem({city}) {
             <span className={styles.emoji}>{emoji}</span>
             <h3 className={styles.cityName}>{cityName}</h3>
             <time className={styles.date}>{formatDate(date)}</time>
-            <button className={styles.deleteBtn}>&times;</button>
+            <button className={styles.deleteBtn} onClick={handleDelete} >&times;</button>
          </Link>
     </li>
   )
